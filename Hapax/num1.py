081e8f2ca3317646a6a01204ec0fc3f727ee1d26
steph45607 <staniswinata.stephanie@gmail.com>
2021-11-08 12:10:10 +0700

    updated

--- Hapax/num1.py
+++ Hapax/num1.py
@@ -5,26 +5,29 @@ os.chdir('/Users/steph/Desktop')
 book  = open('Kafka.txt','r')
 kafka = book.read().lower().replace("\n"," ")
 
-replace =""
-for things in kafka:
-    if things not in string.punctuation:
-        replace += things
-kafka = replace
-wordlist = kafka.split(" ")
+def theHapax(theBook):
+    replace =""
+    for things in theBook:
+        if things not in string.punctuation:
+            replace += things
+    theBook = replace
+    wordlist = theBook.split(" ")
 
-# check the word, so wont check the same word over n over again
-allList = {}
+    # check the word, so wont check the same word over n over again
+    allList = {}
 
-for text in wordlist:
-    if text not in allList:
-        allList[text] = 1
-    else:
-        allList[text] += 1
+    for text in wordlist:
+        if text not in allList:
+            allList[text] = 1
+        else:
+            allList[text] += 1
 
-hapax = []
+    hapax = []
 
-for text in allList:
-    if allList[text] == 1:
-        hapax.append(text)
+    for text in allList:
+        if allList[text] == 1:
+            hapax.append(text)
 
-print(hapax)
+    print(hapax)
+
+theHapax(kafka)
